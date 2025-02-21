7695f21c4d52b30b82cd23e21c7f37f01bcbcae7
ThiagoSaraceni <thiagorx123@gmail.com>
2025-02-21 20:54:49 -0300

    feat: shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+  return (
+    <>
+      <h1 className="text-red-500">products</h1>
+      <Button>Oi</Button>
+      <Input placeholder="teste" />
+    </>
+  );
+};
+
+export default ProductsPage;
