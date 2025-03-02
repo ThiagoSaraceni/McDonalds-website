e4cf2b0fe8da98ab9f8fb72a38fa276e502dce85
ThiagoSaraceni <thiagorx123@gmail.com>
2025-03-02 14:45:26 -0300

    feat: component dine_in and take_away.

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { notFound } from "next/navigation";
 
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 
+import { ConsumptionMethodOption } from "./components/sonsumption-method-option";
+
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
 }
@@ -15,6 +17,7 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
 
   return (
     <div className="px-21 flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* LOGO E TIPO */}
       <div className="flex flex-col items-center gap-2">
         <Image
           src={restaurant?.avatarImageUrl}
@@ -23,6 +26,32 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
           height={82}
         />
         <h2 className="font-semibold">{restaurant?.name}</h2>
+      </div>
+
+      {/* BEM VINDO */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos oferecer
+          praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          buttonText="Para comer aqui"
+          altImg="Comer aqui"
+          imgUrl="/dine_in.png"
+          option="DINE_IN"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          buttonText="Para levar"
+          altImg="Para  levar"
+          imgUrl="/take_away.png"
+          option="TAKEAWAY"
+        />
       </div>
     </div>
   );
