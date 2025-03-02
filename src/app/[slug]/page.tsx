dd5cb42d6f93737e636f3211907d71aae211a4bd
ThiagoSaraceni <thiagorx123@gmail.com>
2025-03-02 10:47:23 -0300

    feat: getlogo url by bd, and config next to accept domain wheres come url.

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
-import { db } from "@/lib/prisma";
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
@@ -7,7 +9,23 @@ interface RestaurantPageProps {
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
   const restaurant = await getRestaurantBySlug(slug);
-  return <h1>{restaurant?.name}</h1>;
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="px-21 flex h-screen flex-col items-center justify-center px-6 pt-24">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant?.avatarImageUrl}
+          alt={restaurant?.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant?.name}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantPage;
